a9fd6ec749e9a987d793a4e225ade0a482e60e41
jayamahendra <jayamahendra490@gmail.com>
2022-04-09 20:42:48 +0700

    bisa login tanpa error dengan user pw yang salah

--- lib/blocs/Auth_bloc.dart
+++ lib/blocs/Auth_bloc.dart
@@ -65,49 +65,4 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       }
     }
   }
-  // Stream<AuthState> mapEventToState(AuthEvent event) async* {
-  //   if (event is AuthCheck) {
-  //     yield AuthLoading();
-
-  //     final hasToken = await authRepository.hasToken();
-  //     if (hasToken != null) {
-  //       yield AuthHasToken(token: hasToken);
-  //     } else {
-  //       yield AuthFailed();
-  //     }
-  //   }
-
-  //   if (event is GetDataWithToken) {
-  //     yield AuthLoading();
-
-  //     final user = await authRepository.getData(event.token);
-  //     yield AuthData(email: user.email, name: user.name);
-  //   }
-
-  //   if (event is LoginProcess) {
-  //     yield AuthLoading();
-  //     try {
-  //       final login =
-  //           await authRepository.loginUser(event.email, event.password);
-  //       if (login.message != "failed") {
-  //         yield LoginSuccess();
-  //         await authRepository.setLocalToken(login.data.token);
-  //         yield AuthHasToken(token: login.data.token);
-  //       }
-  //     } catch (e) {
-  //       yield LoginFailed("Login Gagal");
-  //     }
-  //   }
-
-  //   if (event is LoggedOut) {
-  //     final String token = await authRepository.hasToken();
-  //     try {
-  //       final Logout logout = await authRepository.userLogout(token);
-  //       if (logout.message == "success") {
-  //         await authRepository.unsetLocalToken();
-  //         yield AuthFailed();
-  //       }
-  //     } catch (e) {}
-  //   }
-  // }
 }
